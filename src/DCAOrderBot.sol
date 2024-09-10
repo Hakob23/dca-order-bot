8da83f6ce3b9276abb6e41f63796d725cfc94c0b
Hakob23 <hakob.arshakyan23@gmail.com>
2024-09-10 21:08:16 +0400

    Issue regarding of calling withdrawCollateral with original amountIn value. For now to enable future development

--- src/DCAOrderBot.sol
+++ src/DCAOrderBot.sol
@@ -135,7 +135,7 @@ contract DCAOrderBot {
         });
         calls[1] = MultiCall({
             target: facade,
-            callData: abi.encodeCall(ICreditFacadeV3Multicall.withdrawCollateral, (dcaOrder.tokenIn, amountIn, msg.sender))
+            callData: abi.encodeCall(ICreditFacadeV3Multicall.withdrawCollateral, (dcaOrder.tokenIn, amountIn/4, msg.sender))
         });
         ICreditFacadeV3(facade).botMulticall(dcaOrder.account, calls);
 
